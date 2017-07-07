6aad4211996d85a32355149b69919c78b67f496d
Greg Walker <michael.walker@gsa.gov>
2017-07-07 10:50:15 -0500

    Fix collapse flag

--- web/src/components/dashboard/cms/review-section.js
+++ web/src/components/dashboard/cms/review-section.js
@@ -14,7 +14,7 @@ export default class Section extends React.Component {
 
     this.state = {
       id: `section_${Math.random()}`,
-      expanded: true || !props.collapsed
+      expanded: !props.collapsed
     };
 
     this.getChildren = () => {
