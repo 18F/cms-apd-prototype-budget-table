69de3123a772a642f4a083bcec9836e652ee5121
Greg Walker <michael.walker@gsa.gov>
2017-08-02 15:58:15 -0500

    Add an initial landing page with a clickable map and stuff

--- web/src/components/dashboard/cms/index.js
+++ web/src/components/dashboard/cms/index.js
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
 import Landing from './landing';
+import StateOverview from './state-overview';
 import ReviewRequest from './review';
 
 const render = props => (
   <div className="cms dashboard">
     <Route exact path={`${props.match.path}/`} component={Landing} />
+    <Route exact path={`${props.match.path}/:state`} component={StateOverview} />
     <Route exact path={`${props.match.path}/review/:requestID`} component={ReviewRequest} />
   </div>
 );
