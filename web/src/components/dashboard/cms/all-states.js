d66d487547b965791bb12c95f71baff1c838c7d6
Greg Walker <michael.walker@gsa.gov>
2017-08-04 11:02:10 -0500

    Add a callout box to indicate why the state is highlighted

--- web/src/components/dashboard/cms/all-states.js
+++ web/src/components/dashboard/cms/all-states.js
@@ -3,36 +3,68 @@ import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import Map from 'react-us-state-map';
 
-const render = () => (
-  <div className="cms dashboard all-states">
-    <header className="back"><Link to="/dashboard/cms">Back to the dashboard</Link></header>
-    <header>
-      <h3>All states <small>overview</small></h3>
-    </header>
+class AllStatesView extends React.Component {
+  constructor() {
+    super();
 
-    <div className="summary">
-      <h1>Spending summary</h1>
+    this.state = {
+      hideStateCallout: true
+    };
 
-      <div className="usa-grid">
-        <div className="usa-width-one-half">
-          <h2>FY17 Total CMS allocated funds</h2>
-          <span className="callout">$6,500,293,432.00</span>
+    this.toggleCallout = () => {
+      this.setState({ hideStateCallout: !this.state.hideStateCallout });
+    };
+
+    this.clickState = (state) => {
+      if (state === 'WY') {
+        this.toggleCallout();
+      } else {
+        this.setState({ hideStateCallout: true });
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="cms dashboard all-states">
+        <header className="back"><Link to="/dashboard/cms">Back to the dashboard</Link></header>
+        <header>
+          <h3>All states <small>overview</small></h3>
+        </header>
+
+        <div className="summary">
+          <h1>Spending summary</h1>
+
+          <div className="usa-grid">
+            <div className="usa-width-one-half">
+              <h2>FY17 Total CMS allocated funds</h2>
+              <span className="callout">$6,500,293,432.00</span>
+            </div>
+            <div className="usa-width-one-half">
+              <h2>Total quarterly expenditure to date</h2>
+              <span className="callout">$1,498,735,733.00</span>
+            </div>
+          </div>
         </div>
-        <div className="usa-width-one-half">
-          <h2>Total quarterly expenditure to date</h2>
-          <span className="callout">$1,498,735,733.00</span>
+
+        <div className="map-container">
+          <Map onClick={this.clickState} />
+          <div className="alert-icon" onClick={this.toggleCallout}>!</div> { // eslint-disable-line jsx-a11y/no-static-element-interactions
+          }
+          <div className="state-callout" hidden={this.state.hideStateCallout}>
+            <h2>Wyoming</h2>
+            60-day review period ending
+          </div>
         </div>
+
+        <div className="usa-grid charts">
+          <div className="usa-width-seven-twelfths">hi</div>
+          <div className="usa-width-five-twelfths">bye</div>
+        </div>
+
       </div>
-    </div>
+    );
+  }
+}
 
-    <Map />
-
-    <div className="usa-grid charts">
-      <div className="usa-width-seven-twelfths">hi</div>
-      <div className="usa-width-five-twelfths">bye</div>
-    </div>
-
-  </div>
-);
-
-export default withRouter(render);
+export default withRouter(AllStatesView);
